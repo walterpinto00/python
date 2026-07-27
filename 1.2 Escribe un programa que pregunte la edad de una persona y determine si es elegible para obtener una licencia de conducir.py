6bcbf4fc610241e9f6bff72162fcc2869487419d
walterpinto00 <walterpinto0718@gmail.com>
2026-07-27 12:56:52 -0500

    Solución ejercicio 1.2: Validación de edad para licencia

---
+++ 1.2 Escribe un programa que pregunte la edad de una persona y determine si es elegible para obtener una licencia de conducir.py
@@ -0,0 +1,9 @@
+#Ejercicio:Escribe un programa que pregunte la edad de una persona y determine si es elegible para obtener una licencia de conducir, la licencia se obtiene a partir de los 18 años de edad
+#Autor:Stiven Aparicio Vega
+#Fecha:25/09/2025
+#Entradas
+edad=int(input("Escribe cuantos años tienes: "))
+if(edad>=18):
+    print("Al tener",edad,"años si puedes tener una licencia de conducir")
+else:
+    print("No tienes la edad suficiente para tener una licencia de conducir")
