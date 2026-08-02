f42ba429b9ad3d52fde8527dd83f15f9a408ed51
Walter Fabian Pinto Gutierrez <walterpinto00@github.com>
2026-08-02 00:20:46 -0500

    Solucion ejercicio 2.2: Calculo de descuento en restaurante de Medellin

--- 2.2 Un restaurante en Medellín requiere implementar un programa que calcule el total a pagar por una comida.py
+++ 2.2 Un restaurante en Medellín requiere implementar un programa que calcule el total a pagar por una comida.py
@@ -1,14 +1,14 @@
 #Ejercicio:Un restaurante en Medellín requiere implementar un programa que calcule el total a pagar por una comida, considerando un descuento del 10% los lunes y miércoles para los clientes que consuman más de 50.000 pesos.
-#Autor:Stiven Aparicio Vega
-#Fecha:25/09/2025
+#Autor:Walter Fabian Pinto Gutierrez
+#Fecha:02/08/2026
 #Entradas
-cuenta=float(input("Escribe el valor de la ceunta: "))
-dia=int(input("Si la compra fue hecha un lunes o un miercoles escribe 1, de lo contrario escribe 2: "))
-if(dia==1) and (cuenta>=50000):
-    descuento=cuenta*(10/100)
-    total=cuenta-descuento
-    print("El valor de la cuenta con un 10% de descuento es de:",total)
-elif(cuenta>=0):
-    print("El valor de la cuenta es de:",cuenta)
+cuenta = float(input("Ingrese el valor total de la cuenta: "))
+dia = int(input("Si la compra fue realizada un lunes o miercoles ingrese 1, de lo contrario ingrese 0: "))
+if dia == 1 and cuenta >= 50000:
+    descuento = cuenta * (10 / 100)
+    total = cuenta - descuento
+    print("Tiene un descuento del 10%. El total a pagar es:", total, "pesos")
+elif cuenta > 0:
+    print("No aplica descuento. El total a pagar es:", cuenta, "pesos")
 else:
-    print("Error")
+    print("El valor ingresado no es valido")
