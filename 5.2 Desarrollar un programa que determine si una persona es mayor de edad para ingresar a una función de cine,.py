73509948be5411300562722e9d801f256a0f463e
Walter Fabian Pinto Gutierrez <walterpinto00@github.com>
2026-08-02 00:21:44 -0500

    Solucion ejercicio 5.2: Verificacion de mayor de edad para ingresar al cine

--- 5.2 Desarrollar un programa que determine si una persona es mayor de edad para ingresar a una función de cine,.py
+++ 5.2 Desarrollar un programa que determine si una persona es mayor de edad para ingresar a una función de cine,.py
@@ -1,11 +1,11 @@
 #Ejercicio:Desarrollar un programa que determine si una persona es mayor de edad para ingresar a una función de cine, y en caso contrario, le informe que necesita la compañía de un adulto
-#Autor:Stiven Aparicio Vega
-#Fecha:25/09/2025
+#Autor:Walter Fabian Pinto Gutierrez
+#Fecha:02/08/2026
 #Entradas
-edad=int(input("Escriba su edad: "))
-if(edad>=18):
-    print("Felicidades, cuenta con la edad suficiente")
-elif(edad<18) and (edad>0):
-    print("No puede asistir solo, es necesario que asista con un mayor de edad")
+edad = int(input("Ingrese la edad de la persona: "))
+if edad >= 18:
+    print("La persona tiene", edad, "años. Puede ingresar al cine sin problema")
+elif edad > 0 and edad < 18:
+    print("La persona tiene", edad, "años. Debe ingresar acompañada de un adulto")
 else:
-    print("Error")
+    print("La edad ingresada no es valida")
