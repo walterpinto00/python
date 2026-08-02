48683ac5b65429f882cb806da2621c9a826e65e3
Walter Fabian Pinto Gutierrez <walterpinto00@github.com>
2026-08-02 00:26:21 -0500

    Solucion ejercicio 18.2: Identificacion y validacion de letras vocales

--- 18.2 Escribir un programa que solicite al usuario una letra y, si es una vocal.py
+++ 18.2 Escribir un programa que solicite al usuario una letra y, si es una vocal.py
@@ -1,11 +1,11 @@
-#Ejercicio:Escribir un programa que solicite al usuario una letra y, si es una vocal, muestre el mensaje “Es vocal”. Verificar si el usuario ingresó un string de más de un carácter y, en ese caso, informarle que no se puede procesar el dato.
-#Autor:Stiven Aparicio Vega
-#Fecha:25/09/2025
+#Ejercicio:Escribir un programa que solicite al usuario una letra y, si es una vocal, muestre el mensaje "Es vocal". Verificar si el usuario ingresó un string de más de un carácter y, en ese caso, informarle que no se puede procesar el dato.
+#Autor:Walter Fabian Pinto Gutierrez
+#Fecha:02/08/2026
 #Entradas
-letra=input("Escriba una letra: ")
-if(len(letra)>1):
-    print("No se puede procesar el dato")
-elif(letra=="a") or (letra=="e") or (letra=="i") or (letra=="o") or (letra=="u"):
-    print("Es vocal")
+letra = input("Ingrese una letra para verificar si es vocal: ")
+if len(letra) > 1:
+    print("Solo se puede ingresar un caracter. No es posible procesar el dato ingresado")
+elif letra.lower() in ("a", "e", "i", "o", "u"):
+    print("La letra '", letra, "' es una VOCAL", sep="")
 else:
-    print("No es vocal")
+    print("La letra '", letra, "' es una CONSONANTE", sep="")
