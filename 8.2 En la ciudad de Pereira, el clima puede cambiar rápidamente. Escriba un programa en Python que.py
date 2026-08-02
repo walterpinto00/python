265a32ab43811bad16f9611f61c0a1cf2a55c149
Walter Fabian Pinto Gutierrez <walterpinto00@github.com>
2026-08-02 00:22:46 -0500

    Solucion ejercicio 8.2: Evaluacion y condiciones climaticas en Pereira

--- 8.2 En la ciudad de Pereira, el clima puede cambiar rápidamente. Escriba un programa en Python que.py
+++ 8.2 En la ciudad de Pereira, el clima puede cambiar rápidamente. Escriba un programa en Python que.py
@@ -1,13 +1,13 @@
 #Ejercicio:En la ciudad de Pereira, el clima puede cambiar rápidamente. Escriba un programa en Python que, dada la variable clima determine qué actividad realizará una persona. Si hace sol, la persona irá al parque. Si está nublado pero no llueve, la persona irá al centro comercial. Si está lloviendo, la persona se quedará en casa.
-#Autor:Stiven Aparicio Vega
-#Fecha:25/09/2025
+#Autor:Walter Fabian Pinto Gutierrez
+#Fecha:02/08/2026
 #Entradas
-clima=input("¿Como esta el clima?(sol, nublado, lluvia): ")
-if(clima=="sol"):
-    print("La persona ira al parque")
-elif(clima=="nublado"):
-    print("La persona ira al centro comercial")
-elif(clima == "lluvia") or (clima=="lloviendo"):
-    print("La persona se quedara en la casa")
+clima = input("¿Como esta el clima hoy en Pereira? (sol / nublado / lluvia): ").lower()
+if clima == "sol":
+    print("Hace sol! La persona decidio ir al parque a disfrutar el dia")
+elif clima == "nublado":
+    print("Esta nublado. La persona decidio ir al centro comercial")
+elif clima == "lluvia" or clima == "lloviendo":
+    print("Esta lloviendo. La persona decidio quedarse en casa")
 else:
-    print("Error")
+    print("No se reconoce ese tipo de clima. Ingrese: sol, nublado o lluvia")
