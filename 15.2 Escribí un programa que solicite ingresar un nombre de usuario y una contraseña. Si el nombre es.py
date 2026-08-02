cfa56e8bad8cabf5879021256d5c84080b9b647c
Walter Fabian Pinto Gutierrez <walterpinto00@github.com>
2026-08-02 00:24:58 -0500

    Solucion ejercicio 15.2: Validacion de credenciales de acceso al sistema

--- 15.2 Escribí un programa que solicite ingresar un nombre de usuario y una contraseña. Si el nombre es.py
+++ 15.2 Escribí un programa que solicite ingresar un nombre de usuario y una contraseña. Si el nombre es.py
@@ -1,10 +1,10 @@
-#Ejercicio:Escribí un programa que solicite ingresar un nombre de usuario y una contraseña. Si el nombre es “Gwenevere” y la contraseña es “excalibur”, mostrar en pantalla “Usuario y contraseña correctos. Puede ingresar a Camelot”. Si el nombre o la contraseña no coinciden, mostrar “Acceso denegado”.
-#Autor:Stiven Aparicio Vega
-#Fecha:25/09/2025
+#Ejercicio:Escribí un programa que solicite ingresar un nombre de usuario y una contraseña. Si el nombre es "Gwenevere" y la contraseña es "excalibur", mostrar en pantalla "Usuario y contraseña correctos. Puede ingresar a Camelot". Si el nombre o la contraseña no coinciden, mostrar "Acceso denegado".
+#Autor:Walter Fabian Pinto Gutierrez
+#Fecha:02/08/2026
 #Entradas
-usuario=input("Escriba el nombre de usuario: ")
-clave=input("Escriba la contraseña: ")
-if(usuario=="Gwenevere") and (clave=="excalibur"):
-    print("Usuario y contraseña son correctos, Puede ingresar a Camelot")
+usuario = input("Ingrese su nombre de usuario: ")
+clave = input("Ingrese su contrasena: ")
+if usuario == "Gwenevere" and clave == "excalibur":
+    print("Acceso concedido. Bienvenido/a a Camelot,", usuario)
 else:
-    print("Acceso denegado")
+    print("Acceso denegado. El usuario o la contrasena son incorrectos")
