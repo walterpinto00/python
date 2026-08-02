a574ad083ebd3a4ada888abb2d3dedff687ff1d8
Walter Fabian Pinto Gutierrez <walterpinto00@github.com>
2026-08-02 00:23:45 -0500

    Solucion ejercicio 11.2: Calculo de impuestos segun declaracion de renta

--- 11.2 Los impuestos para la declaración de la renta en un determinado país son los siguientes.py
+++ 11.2 Los impuestos para la declaración de la renta en un determinado país son los siguientes.py
@@ -1,15 +1,20 @@
 #Ejercicio:Los impuestos para la declaración de la renta en un determinado país son los siguientes:
-#Autor:Stiven Aparicio Vega
-#Fecha:25/09/2025
+#Autor:Walter Fabian Pinto Gutierrez
+#Fecha:02/08/2026
 #Entradas
-renta=float(input("Escriba la renta anual: "))
-if(renta<2500000):
-    print("El porcentaje de impuestos es del: 5%")
-elif(renta>=2500000) and (renta<=3500000):
-    print("El porcentaje de impuestos es del: 15%")
-elif(renta>=3500001) and (renta<=4500000):
-    print("El porcentaje de impuestos es del: 20%")
-elif(renta>=4500001) and (renta<=6000000):
-    print("Su porcentaje de impuestos es del: 30%")
+renta = float(input("Ingrese su renta anual en pesos: "))
+if renta < 2500000:
+    porcentaje = 5
+    print("Su renta es de", renta, "pesos. El porcentaje de impuestos es del:", porcentaje, "%")
+elif renta >= 2500000 and renta <= 3500000:
+    porcentaje = 15
+    print("Su renta es de", renta, "pesos. El porcentaje de impuestos es del:", porcentaje, "%")
+elif renta >= 3500001 and renta <= 4500000:
+    porcentaje = 20
+    print("Su renta es de", renta, "pesos. El porcentaje de impuestos es del:", porcentaje, "%")
+elif renta >= 4500001 and renta <= 6000000:
+    porcentaje = 30
+    print("Su renta es de", renta, "pesos. El porcentaje de impuestos es del:", porcentaje, "%")
 else:
-    print("El porcentaje de impuestos es del: 45%")
+    porcentaje = 45
+    print("Su renta es de", renta, "pesos. El porcentaje de impuestos es del:", porcentaje, "%")
